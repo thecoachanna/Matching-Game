2ecd8b347e4748bf969afae798fe11dabba1822f
thecoachanna <nettles.anna@gmail.com>
2022-04-30 18:28:04 -0400

    Cards can turn over

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
-//  1. create class list - const cards = document.querySelectorAll(.memory-card)
-//  2. add event Listener click and flipcards
-//  3. declare flipcard function - this.classList.toggle('flip) (will need to go into css and flip y axis 180 degrees)
 
-// 4. need to know which card is flipped first. Variables for First card, second card. If statement and boolean.
 
-// 5. need a way to make sure cards are the same (IF first card === second card)and keep them flipped (remove EventListener) 
+function pickCard(event) {
 
-// 6.  ELSE unflip the cards (remove the flip class) - will need a set Timeout to give enough time to flip
+    event.target.style.display = 'none';
+    let exerciseImg = event.target.parentElement.querySelector('.exercise');
+    exerciseImg.style.display = 'inline';
+}
 
-// 7. can shorten by making one function (check for match)
-// 8. need to be able to lock the board after cards are flipped so that they can't just keep turning cards over (lock board function)
-// 9. need a way to reset board
-// 10. shuffle cards - generate random number between 1-16 (this will be a function) math.random and math.floor then apply to order property in CSS - wrap this function in parenthesis to make it immediatley invoked
+const cards = document.querySelectorAll('.memory-card');
+cards.forEach(card => card.addEventListener('click', (e) => {
+    return pickCard(e);
 
-//  OR can do a card class list and link cards in an array, iterating through them with a for loop. Use setAttribute
+}));
 
-// make a function to hide dsiplay of front or back when it is clicked to ("flip") the card, using the ID
+let questionMark = document.querySelectorAll('.question-mark');
+let exercise = document.querySelectorAll('.exercise');
+
+let maxTwoTurns = 0;
+let chosenCards = [];
+
+document.body.addEventListener('click', function (e) {
+
+    if (maxTwoTurns < 2) {
+
+        if (e.target.tagName === 'IMG') {
+            chosenCards.push(e.target)
+            if (maxTwoTurns === 1) {
+                // match comparison with ID's e.target.dataset.exercise
+
+            }
+
+            maxTwoTurns = maxTwoTurns + 1;
+        }
+    } else {
+
+    }
+})
+
+function checkForAMatch(chosenCards) {
+    if (chosenCards[0].dataset.exercise === chosenCards[1].dataset.exercise) {
+        alert('You found a match!')
+
+    } else (ifNotAMatch) => {
+
+    }
+}
+
+
+
+
+
+// click counter variable to only allow for two clicks that resets to 0 and removes eventlistener
+// outside of function "clickon" variable
