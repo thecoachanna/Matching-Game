d0101250dd2e0c52c27ae0666ac29809214e7f74
thecoachanna <nettles.anna@gmail.com>
2022-05-06 21:05:55 -0400

    final

--- app.js
+++ app.js
@@ -1,98 +1,79 @@
-const gameBoard = document.getElementById('gameBoard')
-const exerciseCards = gameBoard.children
+// Randomly generating cards. Source:
+// https://stackoverflow.com/questions/315177/any-way-to-shuffle-content-in-multiple-div-elements
 
-let fragment = document.createDocumentFragment() 
+const gameBoard = document.getElementById("gameBoard");
+const exerciseCards = gameBoard.children;
+
+let fragment = document.createDocumentFragment();
 while (exerciseCards.length > 0) {
-    let randomCard = Math.floor(Math.random() * exerciseCards.length)
-    console.log(randomCard)
-    fragment.appendChild(exerciseCards[randomCard])
-    }
+  let randomCard = Math.floor(Math.random() * exerciseCards.length);
+  console.log(randomCard);
+  fragment.appendChild(exerciseCards[randomCard]);
+}
 
-gameBoard.appendChild(fragment)
+gameBoard.appendChild(fragment);
 
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll(".memory-card");
 
-
-
-cards.forEach(card => card.addEventListener('click', (e) => {
+cards.forEach((card) =>
+  card.addEventListener("click", (e) => {
     return pickCard(e);
-
-}));
+  })
+);
 
 let chosenCards = [];
 
+// Allowing to choose no more than 2 cards at a time while checking for match.
+
 function pickCard(event) {
+  event.target.style.display = "none";
 
-    event.target.style.display = 'none';
-
-    let exerciseImg = event.target.parentElement.querySelector('.exercise');
-    exerciseImg.style.display = 'inline';
-    chosenCards.push(exerciseImg)
-    console.log(chosenCards)
-    if (chosenCards.length === 2) {
-        setTimeout(() => {
-            checkForAMatch()
-        }, 1000)
-        
-        
-    }
+  let exerciseImg = event.target.parentElement.querySelector(".exercise");
+  exerciseImg.style.display = "inline";
+  chosenCards.push(exerciseImg);
+  //   console.log(chosenCards);
+  if (chosenCards.length === 2) {
+    setTimeout(() => {
+      checkForAMatch();
+    }, 1000);
+  }
 }
 
-let questionMark = document.querySelectorAll('.question-mark');
-let exercise = document.querySelectorAll('.exercise');
+let questionMark = document.querySelectorAll(".question-mark");
+let exercise = document.querySelectorAll(".exercise");
 
 let maxTwoTurns = 0;
 
+//  Checking for a match and allowing cards to be flipped back over and chosen again if not a match.
 
 function checkForAMatch() {
-    if (chosenCards[0].dataset.exercise === chosenCards[1].dataset.exercise) {
-        alert('You found a match!')
-        foundCards+=2;
-        gameWon()
-
-
-    } else {
-        alert('This is not a match!')
-        // console.log(chosenCards)
-        // chosenCards[0].setAttribute('src', 'images/Question_Mark.png')
-        // chosenCards[1].setAttribute('src', 'images/Question_Mark.png') 
-        console.log(chosenCards[0])
-        chosenCards[0].parentElement.querySelector('.question-mark').style.display = 'inline' 
-        chosenCards[1].parentElement.querySelector('.question-mark').style.display = 'inline'    
-        chosenCards[0].style.display = 'none' 
-        chosenCards[1].style.display = 'none' 
-        
-    }
-    chosenCards = [];
+  if (chosenCards[0].dataset.exercise === chosenCards[1].dataset.exercise) {
+    alert("You found a match!");
+    foundCards += 2;
+    gameWon();
+  } else {
+    alert("This is not a match!");
+    chosenCards[0].parentElement.querySelector(".question-mark").style.display =
+      "inline";
+    chosenCards[1].parentElement.querySelector(".question-mark").style.display =
+      "inline";
+    chosenCards[0].style.display = "none";
+    chosenCards[1].style.display = "none";
+  }
+  chosenCards = [];
 }
 
 const cardsInGame = 16;
 let foundCards = 0;
+const resultDisplay = document.querySelector('#result')
 
 function gameWon() {
-    if (foundCards == cardsInGame){
-        alert('Congrats! You found all of the matches!')
-    }
-    
+  if (foundCards == cardsInGame) {
+    resultDisplay.textContent = "Congratulations! You found all 8 matches!"
+  }
 }
 
-// Need a way to win the game.
-// Need a time delay between flipping cards and finding matches.
-// Need a way to shuffle cards (randomize or sort).
 
-
-
-
-
-
-
-// setTimeout(function (chosenCards) => {
-//             console.log(chosenCards)
-//             chosenCards[0].src = 'images/Question_Mark.png'
-//             chosenCards[1].src = 'images/Question_Mark.png'
-//         }, 2000)
-
-
-
-
-
+// Stretch goals:
+// 1. Add confetti animation once all matching pairs have been found.
+// 2. Clean up CSS to be able to resize and center.
